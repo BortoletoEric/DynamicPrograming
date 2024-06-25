b170894336faee594cb97f6bd935aeb9334caf65
Bortoleto <ericbortoleto_9@outlook.com>
2024-06-25 12:13:47 -0300

    commit02(feat): Implementa Timer ao Calculo

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,6 +3,10 @@ import javax.swing.JOptionPane;
 public class Fibonacci {
     public static void main(String[] args) {
         int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o enezimo numero de Fibonacci desejado"));
+        
+        // inicia timer
+        long inicioTimer = System.nanoTime();
+        
         int a = 0;
         int b = 1;
         int c = 0;
@@ -11,6 +15,14 @@ public class Fibonacci {
             a = b;
             b = c;
         }
+        
+        // finaliza timer
+        long fimTimer = System.nanoTime();
+        
+        // calcula tempo gasto em ms
+        long duration = (fimTimer - inicioTimer) / 1000000; // converte pra ms
+        
         System.out.println(c);
+        System.out.println("Elapsed time: " + duration + " ms");
     }
 }
